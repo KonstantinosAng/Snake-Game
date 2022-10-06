ccad7f9257671eaa92c7b0e6cb8260a8d4bf83f6
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2022-10-06 22:05:38 +0300

    feat(app): - refactor app

--- hiss.js
+++ hiss.js
@@ -1,17 +1,17 @@
 // Store id of elements in html
-var his = document.getElementById('snake_sound');
-var logo = document.getElementById('logo');
+var his = document.getElementById("snake_sound");
+var logo = document.getElementById("logo");
 
 // Variable for snake sound and source of file
 var bleep = new Audio();
-bleep.src = 'sounds/snakehiss.mp3';
+bleep.src = "snakehiss.mp3";
 
 // listen for clicks on snake logo
-logo.addEventListener('click', hiss);
+logo.addEventListener("click", hiss);
 
 // Execute on click on logo
 function hiss() {
-  his.style.display = 'block';
+  his.style.display = "block";
   bleep.play();
   window.setTimeout(fadeout, 1550);
 }
